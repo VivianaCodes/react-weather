65f4e2529ea4b032b23d8a39065a83c1da3cc09d
VivianaCodes <viviana.milone3@gmail.com>
2023-03-20 19:57:16 +0100

    Added Search

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Overview from "./Overview";
+import Search from "./Search";
 
 function App() {
   return (
     <div className="App">
       <div className="weather-app">
         <Overview />
+        <Search />
       </div>
     </div>
   );
