06db02b15faec36d24182e741126fa4cdb047be2
VivianaCodes <viviana.milone3@gmail.com>
2023-04-03 22:45:48 +0200

    adjusted css

--- src/Overview.js
+++ src/Overview.js
@@ -14,38 +14,40 @@ export default function Overview() {
   };
 
   return (
-    <div className="card-now justify-content-center align-items-center">
-      <div className="card-body-now">
-        <h3 className="card-title" id="current-city">
-          {weatherOverview.city}
-        </h3>
-        <div className="current-time">{weatherOverview.date}</div>
-        <h4>
-          <span id="temp-number">{weatherOverview.temperature}</span>
-          <span className="units">
-            <a href="/" id="celsius-link">
-              °C
-            </a>
-            |
-            <a href="/" id="fahrenheit-link">
-              °F
-            </a>
-          </span>
-        </h4>
-        <span id="sky">{weatherOverview.description}</span>
-        <img
-          src={weatherOverview.imgUrl}
-          alt=""
-          id="weather-icon"
-          className="float-left"
-        />
-        <span id="humidity">{weatherOverview.humidity}</span>
-        <span id="wind">{weatherOverview.wind}</span>
-        <form>
-          <button className="btn btn-primary-location" id="search-location">
-            Current
-          </button>
-        </form>
+    <div className="container">
+      <div className="card-now justify-content-center align-items-center">
+        <div className="card-body-now">
+          <h3 className="card-title" id="current-city">
+            {weatherOverview.city}
+          </h3>
+          <div className="current-time">{weatherOverview.date}</div>
+          <h4>
+            <span id="temp-number">{weatherOverview.temperature}</span>
+            <span className="units">
+              <a href="/" id="celsius-link">
+                °C
+              </a>
+              |
+              <a href="/" id="fahrenheit-link">
+                °F
+              </a>
+            </span>
+          </h4>
+          <span id="sky">{weatherOverview.description}</span>
+          <img
+            src={weatherOverview.imgUrl}
+            alt=""
+            id="weather-icon"
+            className="float-left"
+          />
+          <span id="humidity">{weatherOverview.humidity}</span>
+          <span id="wind">{weatherOverview.wind}</span>
+          <form>
+            <button className="btn btn-primary-location" id="search-location">
+              Current
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
